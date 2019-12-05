1f628f168445a17f901cd70328ebf067173e2982
Eric Vincent <eric@efvincent.com>
2019-12-05 01:24:49 -0500

    tidy up

--- day02.fsx
+++ day02.fsx
@@ -1,23 +1,17 @@
 open System
 
-let raw = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,19,5,23,2,13,23,27,1,10,27,31,2,6,31,35,1,9,35,39,2,10,39,43,1,43,9,47,1,47,9,51,2,10,51,55,1,55,9,59,1,59,5,63,1,63,6,67,2,6,67,71,2,10,71,75,1,75,5,79,1,9,79,83,2,83,10,87,1,87,6,91,1,13,91,95,2,10,95,99,1,99,6,103,2,13,103,107,1,107,2,111,1,111,9,0,99,2,14,0,0
-"
-let crashed = raw.Split(',') |> Seq.mapi(fun i n -> (i,int n)) |> Map.ofSeq
-let restored = crashed.Add(1,12).Add(2,2)
+let restored =
+    let raw = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,19,5,23,2,13,23,27,1,10,27,31,2,6,31,35,1,9,35,39,2,10,39,43,1,43,9,47,1,47,9,51,2,10,51,55,1,55,9,59,1,59,5,63,1,63,6,67,2,6,67,71,2,10,71,75,1,75,5,79,1,9,79,83,2,83,10,87,1,87,6,91,1,13,91,95,2,10,95,99,1,99,6,103,2,13,103,107,1,107,2,111,1,111,9,0,99,2,14,0,0"
+    let crashed = raw.Split(',') |> Seq.mapi(fun i n -> (i,int n)) |> Map.ofSeq
+    crashed.Add(1,12).Add(2,2)
 
-let toStr prog =
-    String.Join(',', Map.toSeq prog |> Seq.map (fun (_,v) -> string v))
-
-let rec run pos (prog:Map<int,int>) =    
-    if prog.[pos] <> 99 then
-        let (opcode,v1,v2,addrRet) = 
-            prog.[pos], prog.[prog.[pos+1]], prog.[prog.[pos+2]], prog.[pos+3]
-        match opcode with
-        | 1 -> run (pos+4) (prog.Add(addrRet, (v1 + v2)))
-        | 2 -> run (pos+4) (prog.Add(addrRet, (v1 * v2)))
-        | n -> failwith (sprintf "invalid operation %i at position %i" n pos)
-    else
-        prog
+let rec run p (mem:Map<int,int>) =    
+    let (op,v1,v2,pRet) = mem.[p], mem.[mem.[p+1]], mem.[mem.[p+2]], mem.[p+3]
+    match op with
+    | 1 -> run (p+4) (mem.Add(pRet, (v1 + v2)))
+    | 2 -> run (p+4) (mem.Add(pRet, (v1 * v2)))
+    | 99 -> mem
+    | n -> failwith (sprintf "invalid operation %i at position %i" n p)
 
 printfn "Day 2 part 1: %i" (run 0 restored).[0]
 
